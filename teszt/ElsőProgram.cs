6866ae4904f7c38461af29d700aed34593764240
2022099 <2022099@B104-16>
2024-10-04 14:18:20 +0200

    Add project files.

---
+++ teszt/ElsőProgram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teszt
+{
+    class ElsőProgram 
+    {
+        static void Main(string[] args)
+        {
+            // Ide jönnek a kódok
+        }
+    }
+}
