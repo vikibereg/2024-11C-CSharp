01970e5aa961abfb283fbf930e71a2b909c96380
vikibereg <viktoriaandorko1214@gmail.com>
2024-10-09 14:43:02 +0200

    teszt

--- teszt/ElsőProgram.cs
+++ teszt/ElsőProgram.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +10,14 @@ namespace teszt
     {
         static void Main(string[] args)
         {
-            // Ide jönnek a kódok
+            int x = 100,
+                     y = 11;
+
+            Console.WriteLine($ "1.iratás x = {x}, x++ = {x++}");
+            Console.WriteLine($"2.iratás x = {x}, x++ = {x++}");
+            Console.WriteLine($"1. iratás y = {y}, ++y = {++y}");
+            Console.WriteLine($"2. iratás y = {y}, ++y = {++y}");
+            Console.ReadKey();
         }
     }
 }
